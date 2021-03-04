0dd58d7bebc7468def2ca1f6b5047ae24a053d03
iambadboy317 <80063809+iambadboy317@users.noreply.github.com>
2021-03-04 21:36:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/SbjYRhrKCb`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("755636700482633791").send(
+  client.channels.get("817048878288601109").send(
 `> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -57,4 +57,4 @@ client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑�
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("ODE3MDk0NzIyMzI1NzA4ODAx.YEEgwg.IAKAiCYSupYDYFc8e_QWLBm7eWc");//تۆکین لێرە دانێ
